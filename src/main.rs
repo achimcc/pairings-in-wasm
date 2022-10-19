10bb58540ea5a876911fdbad48454021a26e4298
Achim Schneider <achim.schneider@posteo.de>
2022-10-19 16:08:31 +0200

    do pairing

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,6 @@ fn main() -> Result<()> {
 
     let module = Module::new(&engine, &mut &wasm[..])?;
 
-
     // All Wasm objects operate within the context of a `Store`.
     // Each `Store` has a type parameter to store host-specific data,
     // which in this case we are using `42` for.
@@ -26,19 +25,49 @@ fn main() -> Result<()> {
 
     // Instantiate memory object and link it:
     let memory = Memory::new(&mut store, MemoryType::new(25, None));
-    linker.define("env", "memory", memory.unwrap())?;
+    let memory = memory.unwrap();
+    linker.define("env", "memory", memory)?;
 
     // Instantiate
     let instance = linker.instantiate(&mut store, &module)?.start(&mut store)?;
-     
-    // Export WASM ccall
-    let square = instance
-        .get_export(&store, "f2m_square")
-        .and_then(Extern::into_func)
-        .ok_or_else(|| anyhow!("could not find function \"f2m_square\""))?
-        .typed::<(i32, i32), ()>(&mut store)?;
-    // And finally we can call the wasm!
 
-    square.call(&mut store, (5, 4))?;
+    // Export WASM call for pairing
+    let compute_pairing = instance
+        .get_export(&store, "bls12381_pairing")
+        .and_then(Extern::into_func)
+        .ok_or_else(|| anyhow!("could not find function \"bls12381_pairing\""))?
+        .typed::<(i32, i32, i32), ()>(&mut store)?;
+
+    compute_pairing.call(&mut store, (42104, 42392, 92000))?;
+
+    // export WASM call for conversion from Montgomery
+    let from_montgomery = instance
+        .get_export(&store, "ftm_fromMontgomery")
+        .and_then(Extern::into_func)
+        .ok_or_else(|| anyhow!("could not find function \"ftm_fromMontgomery\""))?
+        .typed::<(i32, i32), ()>(&mut store)?;
+        from_montgomery.call(&mut store, (92000, 92000))?;
+
+    // read data at result location
+    let data = memory.data(&store).clone();
+    let result = [
+        [
+            [shift(data, 0), shift(data, 1)],
+            [shift(data, 2), shift(data, 3)],
+            [shift(data, 4), shift(data, 5)],
+        ],
+        [
+            [shift(data, 6), shift(data, 7)],
+            [shift(data, 8), shift(data, 9)],
+            [shift(data, 10), shift(data, 11)],
+        ],
+    ];
+    println!("result: {:?}", result);
     Ok(())
+}
+
+fn shift(data: &[u8], pos: usize) -> &[u8] {
+    let start: usize = 92000;
+    let n8q: usize = 96;
+    &data[(start + pos * n8q * 5)..92000 + ((start + 1) * 5*  n8q - 1)]
 }
